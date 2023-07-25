8764c03086cee26445960f61a9383591b075cc66
Alex Mit <klepoz.672@mail.ru>
2023-07-26 01:07:09 +0300

    gitsigns added

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,6 @@
 return {
     'tpope/vim-fugitive',
     'TimUntersberger/neogit',
-    { 'kdheepak/lazygit.nvim', dependencies = { 'nvim-lua/plenary.nvim', }, },
+    { 'kdheepak/lazygit.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'lewis6991/gitsigns.nvim', opts = {} },
 }
