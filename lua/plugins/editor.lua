561a7cb0f433ae4cdd01148ee96857f44202982a
Alex Mit <klepoz.672@mail.ru>
2023-07-26 01:08:30 +0300

    toggleterm added

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -198,4 +198,17 @@ return {
             vim.g.loaded_netrwPlugin = 1
         end,
     },
+    {
+        'akinsho/toggleterm.nvim', version = "*", opts = {
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                end
+            end,
+            open_mapping = [[<c-\>]],
+            hide_numbers = true, -- hide the number column in toggleterm buffers
+        }
+    }
 }
