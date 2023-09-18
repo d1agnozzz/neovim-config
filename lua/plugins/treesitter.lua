a486d540be97cd1aca84008b8a35b58cb509d4ab
Alex Mit <klepoz.672@mail.ru>
2023-09-18 13:57:46 +0300

    nvim: tressiter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,53 +1,73 @@
-return  {
+---@diagnostic disable: missing-fields
+return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs',
-    opts = {
-        ensure_installed = {
-            'c',
-            'lua',
-            'vim',
-            'vimdoc',
-            'query',
-            'rust',
-            'python',
-            'markdown',
-            'markdown_inline',
-        },
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = {
+                'c',
+                'lua',
+                'vim',
+                'vimdoc',
+                'query',
+                'rust',
+                'python',
+                'markdown',
+                'markdown_inline',
+                'tsx',
+                'json',
+                'yaml',
+                'html',
+                'css',
+            },
 
-        sync_install = false,
+            sync_install = false,
 
-        auto_install = true,
+            auto_install = true,
 
-        highlight = {
-            enable = true,
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-    },
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then return true end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            autotag = {
+                enable = true,
+            },
+            endwise = {
+                enable = true,
+            },
+            rainbow = {
+                enable = true,
+                -- disable = { 'jsx', 'cpp', 'tsx', 'ts' }, -- list of languages you want to disable the plugin for
+                query = {
+                    'rainbow-parens',
+                    html = 'rainbow-tags',
+                    tsx = 'rainbow-parens-react',
+                },
+                strategy = require('ts-rainbow').strategy['global'],
+            },
+        })
+        --         require('tree-sitter-typescript').typescript; -- TypeScript grammar
+        -- require('tree-sitter-typescript'); -- TSX grammar
+    end,
     dependencies = {
         {
             'HiPhish/nvim-ts-rainbow2',
-            config = function()
-                require('nvim-treesitter.configs').setup {
-
-                    rainbow = {
-                        enable = true,
-                        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-                        query = 'rainbow-parens',
-                        strategy = require('ts-rainbow').strategy[ 'global' ],
-                    }
-                }
-            end
+            -- config = function()
+            --     require('nvim-treesitter.configs').setup({
+            --
+            --     })
+            -- end,
         },
         'nvim-treesitter/nvim-treesitter-context',
         'JoosepAlviste/nvim-ts-context-commentstring',
-    }
+        'windwp/nvim-ts-autotag',
+        'RRethy/nvim-treesitter-endwise',
+    },
 }
