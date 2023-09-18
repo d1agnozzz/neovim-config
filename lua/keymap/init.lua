b4108584259c7cba0b65bc3559c0bd111c19d433
Alex Mit <klepoz.672@mail.ru>
2023-09-18 13:55:42 +0300

    nvim: keymap changes

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -9,32 +9,33 @@ require('keymap.plugins.autopairs')
 require('keymap.plugins.trouble')
 require('keymap.plugins.formatter')
 require('keymap.plugins.dap')
+require('keymap.plugins.ufo')
 
 -- Yank to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
 
 -- Swap lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
--- Keep selection after indenting 
+-- Keep selection after indenting
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
 
 -- Write changes, close buffer and open second last opened buffer
 -- vim.keymap.set('n', '<leader>wq', ':w<bar>bd<bar>b#<bar>b#<cr>')
 vim.keymap.set('n', '<leader>d', function()
-  -- Check if the current buffer is the last one
-  if #vim.fn.getbufinfo({buflisted = true}) == 1 then
-    -- Open nvim-tree
-    vim.cmd('w|bd|NvimTreeToggle')
+    -- Check if the current buffer is the last one
+    if #vim.fn.getbufinfo({ buflisted = true }) == 1 then
+        -- Open nvim-tree
+        vim.cmd('w|bd|NvimTreeToggle')
     -- Alternatively, you can close neovim using the following command
     -- vim.cmd('qa')
-  else
-    -- Save and close the current buffer
-    vim.cmd('w|bd|b#|b#')
-  end
+    else
+        -- Save and close the current buffer
+        vim.cmd('w|bd|b#|b#')
+    end
 end)
 vim.keymap.set('n', '<c-s>', ':w<cr>')
 vim.keymap.set('n', '<c-q>', ':qa!<cr>')
