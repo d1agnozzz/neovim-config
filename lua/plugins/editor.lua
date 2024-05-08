e3ebeecaca75b9b4ce9acc522f47f72702cbd4ab
Alex Mit <klepoz.672@mail.ru>
2024-05-08 11:31:41 +0300

    nvim: add outline plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -392,4 +392,16 @@ return {
             require('alpha').setup(require('alpha.themes.dashboard').config)
         end,
     },
+    {
+        "hedyhli/outline.nvim",
+        config = function()
+            -- Example mapping to toggle outline
+            vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+                { desc = "Toggle Outline" })
+
+            require("outline").setup {
+                -- Your setup opts here (leave empty to use defaults)
+            }
+        end,
+    },
 }
