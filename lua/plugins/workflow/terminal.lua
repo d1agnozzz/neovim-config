cd0165cabb7e7ef734a93d8980cd4fdd6a1b1fcc
Mitrofanov Aleksej Olegovich <amitrofanovol@ozon.ru>
2026-04-06 23:54:23 +0300

    toggleterm: floating windows

--- lua/plugins/workflow/terminal.lua
+++ lua/plugins/workflow/terminal.lua
@@ -3,13 +3,17 @@ return {
         'akinsho/toggleterm.nvim',
         version = '*',
         opts = {
-            size = function(term)
-                if term.direction == 'horizontal' then
-                    return 15
-                elseif term.direction == 'vertical' then
-                    return vim.o.columns * 0.4
-                end
-            end,
+            -- size = function(term)
+            --     if term.direction == 'horizontal' then
+            --         return 15
+            --     elseif term.direction == 'vertical' then
+            --         return vim.o.columns * 0.4
+            --     end
+            -- end,
+            direction = 'float',
+            float_opts = {
+                border = 'curved',
+            },
             open_mapping = [[<c-\>]],
             hide_numbers = true, -- hide the number column in toggleterm buffers
         },
